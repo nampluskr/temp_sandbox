a4020293236bff24245498991fe6febd401d8650
nampluskr <37031766+nampluskr@users.noreply.github.com>
2025-12-23 17:55:10 +0900

    Create trainer.py

---
+++ vad_mini/20251223/stfpm/trainer.py
@@ -0,0 +1,34 @@
+# src/vad_mini/models/stfpm/trainer.py
+
+import torch
+import torch.optim as optim
+
+from vad_mini.models.components.base_trainer import BaseTrainer
+from .torch_model import STFPMModel
+from .loss import STFPMLoss
+
+
+class STFPMTrainer(BaseTrainer):
+    def __init__(self, backbone="resnet50", layers=["layer1", "layer2", "layer3"]):
+
+        model = STFPMModel(
+            backbone=backbone,
+            layers=layers,
+        )
+        loss_fn = STFPMLoss()
+        super().__init__(model, loss_fn)
+
+
+    def configure_optimizers(self):
+        self.optimizer = optim.SGD(
+            params=self.model.student_model.parameters(),
+            lr=0.4,
+            momentum=0.9,
+            dampening=0.0,
+            weight_decay=0.001,
+        )
+
+    def training_step(self, batch):
+        images = batch["image"].to(self.device)
+        loss = self.loss_fn(*self.model(images))
+        return {"loss": loss}
