6bd7bf92922d5f19b08df9963c446fca99c1aa3d
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-01-05 12:53:37 +0900

    Create model_trainer_fastflow.py

---
+++ defectvad/20260105/models/model_trainer_fastflow.py
@@ -0,0 +1,48 @@
+# src/defectvad/models/fastflow/trainer.py
+
+import torch
+import torch.optim as optim
+
+from defectvad.common.base_model import BaseModel
+from defectvad.common.base_trainer import BaseTrainer
+
+from .torch_model import FastflowModel
+from .loss import FastflowLoss
+
+
+class Fastflow(BaseModel):
+    def __init__(self, backbone="wide_resnet50_2",
+        flow_steps=8, conv3x3_only=False, hidden_ratio=1.0, input_size=(256, 256)):
+
+        model = FastflowModel(
+            input_size=input_size,
+            backbone=backbone,
+            pre_trained=True,
+            flow_steps=flow_steps,
+            conv3x3_only=conv3x3_only,
+            hidden_ratio=hidden_ratio,
+        )
+        super().__init__(model)
+
+
+class FastflowTrainer(BaseTrainer):
+    def __init__(self, model):
+
+        if not isinstance(model, Fastflow):
+            raise TypeError(f"Unexpected  model: {type(model).__name__}")
+
+        loss_fn = FastflowLoss()
+        super().__init__(model, loss_fn=loss_fn)
+
+    def configure_optimizers(self):
+        self.optimizer = optim.Adam(
+            params=self.model.parameters(),
+            lr=0.001,
+            weight_decay=0.00001,
+        )
+
+    def training_step(self, batch):
+        images = batch["image"].to(self.device)
+        hidden_variables, jacobians = self.model(images)
+        loss = self.loss_fn(hidden_variables, jacobians)
+        return {"loss": loss}
